e85e31e81d70bbe78b6d71ac4977ad7c655ab2d1
adarsh1159 <adarsh17082006@gmail.com>
2026-08-31 08:35:03 +0530

    Add

--- stronglyconnectedtarjan.cpp
+++ stronglyconnectedtarjan.cpp
@@ -1,68 +1,69 @@
-// Strongly Connected Components (Kosaraju)
+// Strongly Connected Components (Tarjan)
 // TC: O(V + E)
-// SC: O(V + E)
+// SC: O(V)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 class SCC {
 public:
-    void dfs1(int node, vector<vector<int>>& adj, vector<int>& vis,
-              stack<int>& st) {
-        vis[node] = 1;
+    int timer = 0;
+
+    void dfs(int node, vector<vector<int>>& adj,
+             vector<int>& tin, vector<int>& low,
+             vector<int>& inStack, stack<int>& st,
+             vector<vector<int>>& sccs) {
+
+        tin[node] = low[node] = timer++;
+        st.push(node);
+        inStack[node] = 1;
 
         for (int nei : adj[node]) {
-            if (!vis[nei])
-                dfs1(nei, adj, vis, st);
+
+            // Back edge to a node currently in stack
+            if (inStack[nei]) {
+                low[node] = min(low[node], tin[nei]);
+            }
+
+            // Unvisited node
+            else if (tin[nei] == -1) {
+                dfs(nei, adj, tin, low, inStack, st, sccs);
+
+                low[node] = min(low[node], low[nei]);
+            }
         }
 
-        st.push(node);
-    }
+        // node is the root of an SCC
+        if (low[node] == tin[node]) {
+            vector<int> component;
 
-    void dfs2(int node, vector<vector<int>>& rev,
-              vector<int>& vis, vector<int>& component) {
-        vis[node] = 1;
-        component.push_back(node);
+            while (true) {
+                int curr = st.top();
+                st.pop();
+                inStack[curr] = 0;
 
-        for (int nei : rev[node]) {
-            if (!vis[nei])
-                dfs2(nei, rev, vis, component);
+                component.push_back(curr);
+
+                if (curr == node)
+                    break;
+            }
+
+            sccs.push_back(component);
         }
     }
 
     vector<vector<int>> findSCC(int V, vector<vector<int>>& adj) {
-        vector<int> vis(V, 0);
+
+        vector<int> tin(V, -1);
+        vector<int> low(V, -1);
+        vector<int> inStack(V, 0);
+
         stack<int> st;
-
-        // Step 1: Get finishing order
-        for (int i = 0; i < V; i++) {
-            if (!vis[i])
-                dfs1(i, adj, vis, st);
-        }
-
-        // Step 2: Reverse graph
-        vector<vector<int>> rev(V);
-
-        for (int u = 0; u < V; u++) {
-            for (int v : adj[u]) {
-                rev[v].push_back(u);
-            }
-        }
-
-        // Step 3: DFS in finishing order on reversed graph
-        fill(vis.begin(), vis.end(), 0);
-
         vector<vector<int>> sccs;
 
-        while (!st.empty()) {
-            int node = st.top();
-            st.pop();
-
-            if (!vis[node]) {
-                vector<int> component;
-                dfs2(node, rev, vis, component);
-                sccs.push_back(component);
-            }
+        for (int i = 0; i < V; i++) {
+            if (tin[i] == -1)
+                dfs(i, adj, tin, low, inStack, st, sccs);
         }
 
         return sccs;
